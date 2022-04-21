dd279a6cec306edf56ef92e74f2e6cb3012eebf9
Mphatso Lichapa <lichapau@gmail.com>
2022-04-21 14:48:55 +0200

    Created factorial method

--- solver.rb
+++ solver.rb
@@ -1,3 +1,11 @@
 class Solver
-  def factorial; end
+  def factorial(num) 
+    if num.zero?
+      1
+    elsif num.negative?
+      raise ArgumentError
+    else
+      num * factorial(num - 1)
+    end
+  end
 end
