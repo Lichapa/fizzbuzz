84c7a328cbfaec489dc7e01d0baadc7ff2fbe26a
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-21 19:18:18 +0500

    :art: Added fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -12,4 +12,16 @@ class Solver
   def reverse(str)
     str.chars.reverse.join
   end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'FizzBuzz'
+    elsif (num % 3).zero?
+      'Fizz'
+    elsif (num % 5).zero?
+      'Buzz'
+    else
+      num.to_s
+    end
+  end
 end
