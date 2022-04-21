81d1c8a0aa210bd20c0955a25e56c457efdc2612
Mphatso Lichapa <lichapau@gmail.com>
2022-04-21 14:18:37 +0200

    Created test for instance of class Solver

---
+++ spec/solver_spec.rb
@@ -0,0 +1,13 @@
+require './solver'
+
+describe Solver do
+  before :each do
+    @solver = Solver.new 
+  end
+
+  it 'has instance of class Solver' do
+    expect(@solver).to be_instance_of Solver
+  end
+
+
+end
